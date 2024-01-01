a20642043d1e3079e0f583d339ed6d17d9f794c1
Yasshi-cookie <y.teruyacookie@gmail.com>
2024-01-01 15:53:35 +0900

    拡張子をjsからjsxに変更

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -4,43 +4,49 @@ import Button from '@/components/Button'
 import Input from '@/components/Input'
 import InputError from '@/components/InputError'
 import Label from '@/components/Label'
+import Link from 'next/link'
 import { useAuth } from '@/hooks/auth'
 import { useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import AuthSessionStatus from '@/app/(auth)/AuthSessionStatus'
 
-const PasswordReset = () => {
-    const searchParams = useSearchParams()
+const Login = () => {
+    const router = useRouter()
 
-    const { resetPassword } = useAuth({ middleware: 'guest' })
+    const { login } = useAuth({
+        middleware: 'guest',
+        redirectIfAuthenticated: '/dashboard',
+    })
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [passwordConfirmation, setPasswordConfirmation] = useState('')
+    const [shouldRemember, setShouldRemember] = useState(false)
     const [errors, setErrors] = useState([])
     const [status, setStatus] = useState(null)
 
-    const submitForm = event => {
+    useEffect(() => {
+        if (router.reset?.length > 0 && errors.length === 0) {
+            setStatus(atob(router.reset))
+        } else {
+            setStatus(null)
+        }
+    })
+
+    const submitForm = async event => {
         event.preventDefault()
 
-        resetPassword({
+        login({
             email,
             password,
-            password_confirmation: passwordConfirmation,
+            remember: shouldRemember,
             setErrors,
             setStatus,
         })
     }
 
-    useEffect(() => {
-        setEmail(searchParams.get('email'))
-    }, [searchParams.get('email')])
-
     return (
         <>
-            {/* Session Status */}
             <AuthSessionStatus className="mb-4" status={status} />
-
             <form onSubmit={submitForm}>
                 {/* Email Address */}
                 <div>
@@ -62,6 +68,7 @@ const PasswordReset = () => {
                 {/* Password */}
                 <div className="mt-4">
                     <Label htmlFor="password">Password</Label>
+
                     <Input
                         id="password"
                         type="password"
@@ -69,43 +76,45 @@ const PasswordReset = () => {
                         className="block mt-1 w-full"
                         onChange={event => setPassword(event.target.value)}
                         required
+                        autoComplete="current-password"
                     />
 
-                    <InputError
-                        messages={errors.password}
-                        className="mt-2"
-                    />
+                    <InputError messages={errors.password} className="mt-2" />
                 </div>
 
-                {/* Confirm Password */}
-                <div className="mt-4">
-                    <Label htmlFor="passwordConfirmation">
-                        Confirm Password
-                    </Label>
+                {/* Remember Me */}
+                <div className="block mt-4">
+                    <label
+                        htmlFor="remember_me"
+                        className="inline-flex items-center">
+                        <input
+                            id="remember_me"
+                            type="checkbox"
+                            name="remember"
+                            className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            onChange={event =>
+                                setShouldRemember(event.target.checked)
+                            }
+                        />
 
-                    <Input
-                        id="passwordConfirmation"
-                        type="password"
-                        value={passwordConfirmation}
-                        className="block mt-1 w-full"
-                        onChange={event =>
-                            setPasswordConfirmation(event.target.value)
-                        }
-                        required
-                    />
-
-                    <InputError
-                        messages={errors.password_confirmation}
-                        className="mt-2"
-                    />
+                        <span className="ml-2 text-sm text-gray-600">
+                            Remember me
+                        </span>
+                    </label>
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
-                    <Button>Reset Password</Button>
+                    <Link
+                        href="/forgot-password"
+                        className="underline text-sm text-gray-600 hover:text-gray-900">
+                        Forgot your password?
+                    </Link>
+
+                    <Button className="ml-3">Login</Button>
                 </div>
             </form>
         </>
     )
 }
 
-export default PasswordReset
+export default Login
