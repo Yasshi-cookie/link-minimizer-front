de92b0f87f88bffcdd07c7edc11394cce8aa9feb
Yasshi-cookie <y.teruyacookie@gmail.com>
2024-05-12 00:48:16 +0900

    TODOコメント追加

--- tests/BasicTest.spec.js
+++ tests/BasicTest.spec.js
@@ -1,12 +1,14 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
 
+// TODO: CI/CDに組み込んだ際に、APIを叩く処理でこけているので、
+// その部分をモック化するかテスト環境のエンドポイントでテストする
 test('has title', async ({ page }, testInfo) => {
     await page.goto('/login')
     await expect(page).toHaveTitle('URLカンタン短縮')
 
     // メールアドレスとパスワードを入力
-    await page.getByLabel('email').fill('sample@sample.com')
+    await page.getByLabel('email').fill('test@sample.com')
     await page.getByLabel('password').fill('password')
     await page.getByRole('button', { name: 'Login' }).click()
 
